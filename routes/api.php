aec6ec493308f98b8b9288776c8b524eb983f7bd
saimhashmii <saimhashmii433@gmail.com>
2022-05-30 12:51:16 +0500

    update

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+  'prefix' => 'v1', 
+  'as' => 'api.', 
+  'namespace' => 'Api\V1\Admin', 
+  'middleware' => ['auth:api']
+], function () {
+    Route::apiResource('tasks', 'TasksApiController');
+});
